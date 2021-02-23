0aea4f7ea23adb265471a581c100dd405b916797
rapha <raphaelalvesoliver998@gmail.com>
2021-02-23 20:14:24 -0300

    Alterações

--- Argis/argis.php
+++ Argis/argis.php
@@ -1,3 +1,9 @@
+<?php
+
+  /* Template Name: Argis */
+  
+?>
+
 <!doctype html>
 <html>
 
@@ -8,17 +14,17 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="assets/css/bootstrap.css">
-  <link rel="stylesheet" href="assets/css/lightbox.min.css">
-  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="<?php bloginfo( 'template_url' );?>/assets/css/bootstrap.css">
+  <link rel="stylesheet" href="<?php bloginfo( 'template_url' );?>/assets/css/lightbox.min.css">
+  <link rel="stylesheet" href="<?php bloginfo( 'template_url' );?>/assets/css/style.css">
 </head>
 
 <body>
   <div class="container">
     <div class="row">
       <div class="col-12 d-flex justify-content-end p-3">
-        <img class="idioma mr-4 " src="assets/img/br.svg" alt="Português">
-        <img class="idioma" src="assets/img/american.svg" alt="Inglês">
+        <img class="idioma mr-4 " src="<?php bloginfo( 'template_url' );?>/assets/img/br.svg" alt="Português">
+        <img class="idioma" src="<?php bloginfo( 'template_url' );?>/assets/img/american.svg" alt="Inglês">
       </div>
     </div>
   </div>
@@ -26,7 +32,7 @@
 
     <div class="col-5 col-sm-6 col-xl-6  p-0">
       <a class="navbar-brand pl-xl-5" href="#">
-        <img class="logo" src="assets/img/logo.svg" width="100" height="100" alt="Argis">
+        <img class="logo" src="<?php bloginfo( 'template_url' );?>/assets/img/logo.svg" width="100" height="100" alt="Argis">
       </a>
     </div>
     <div class="col-7 col-sm-6 col-xl-6 p-0 ">
@@ -58,18 +64,18 @@
         <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
           <div class="carousel-inner">
             <div class="carousel-item active">
-              <a href="assets/img/Açaii_Jaraguaa.png" data-lightbox="img1" data-title="Açaí Jaraguaré">
-                <img class="d-block w-100" src="assets/img/Açaii_Jaraguaa.png" alt="First slide">
+              <a href="<?php bloginfo( 'template_url' );?>/assets/img/Açaii_Jaraguaa.png" data-lightbox="img1" data-title="Açaí Jaraguaré">
+                <img class="d-block w-100" src="<?php bloginfo( 'template_url' );?>/assets/img/Açaii_Jaraguaa.png" alt="First slide">
               </a>
             </div>
             <div class="carousel-item">
-              <a href="assets/img/alto_pinheiros.jpg" data-lightbox="img2" data-title="Alto Pinheiro">
-                <img class="d-block w-100" src="assets/img/alto_pinheiros.jpg" alt="Second slide">
+              <a href="<?php bloginfo( 'template_url' );?>/assets/img/alto_pinheiros.jpg" data-lightbox="img2" data-title="Alto Pinheiro">
+                <img class="d-block w-100" src="<?php bloginfo( 'template_url' );?>/assets/img/alto_pinheiros.jpg" alt="Second slide">
               </a>
             </div>
             <div class="carousel-item">
               <a href="assets/img/best-center.png" data-lightbox="img3" data-title="Best Center">
-                <img class="d-block w-100" src="assets/img/best-center.png" alt="Third slide">
+                <img class="d-block w-100" src="<?php bloginfo( 'template_url' );?>/assets/img/best-center.png" alt="Third slide">
               </a>
             </div>
           </div>
@@ -86,10 +92,10 @@
     </div>
     <div class="row row-cols-2 mt-5 mt-sm-3 mb-5 mb-sm-3 mt-md-5  mt-lg-5 mb-lg-5 mt-xl-0 mb-lg-0 mb-xl-0">
       <div class="col">
-        <img class="icon float-right" src="assets/img/fb.svg" alt="Facebook">
+        <img class="icon float-right" src="<?php bloginfo( 'template_url' );?>/assets/img/fb.svg" alt="Facebook">
       </div>
       <div class="col">
-        <img class="icon float-left" src="assets/img/instagram.svg" alt="Instagram">
+        <img class="icon float-left" src="<?php bloginfo( 'template_url' );?>/assets/img/instagram.svg" alt="Instagram">
       </div>
     </div>
   </div>
@@ -97,130 +103,130 @@
     <div class="grid">
       <div class="grid-sizer"></div>
       <div class="grid-item">
-        <a href="assets/img/Açaii_Jaraguaa.png" data-lightbox="img1" data-title="Açaí Jaraguaré">
-            <img src="assets/img/Açaii_Jaraguaa.png" alt="Açaí Jaraguaré">
+        <a href="<?php bloginfo( 'template_url' );?>/assets/img/Açaii_Jaraguaa.png" data-lightbox="img1" data-title="Açaí Jaraguaré">
+            <img src="<?php bloginfo( 'template_url' );?>/assets/img/Açaii_Jaraguaa.png" alt="Açaí Jaraguaré">
             <p class="text-item">Açaí Jaraguaré</p>
         </a>
       </div>
       <div class="grid-item grid-item--width2">
-        <a href="assets/img/best-center.png" data-lightbox="img2"
+        <a href="<?php bloginfo( 'template_url' );?>/assets/img/best-center.png" data-lightbox="img2"
           data-title="Best Center - Capão Redondo">
-          <img src="assets/img/best-center.png" alt="Best Center">
+          <img src="<?php bloginfo( 'template_url' );?>/assets/img/best-center.png" alt="Best Center">
           <p class="text-item">Best Center</p>
         </a>
       </div>
       <div class="grid-item">
-        <a href="assets/img/ilha.png" data-lightbox="img3"
+        <a href="<?php bloginfo( 'template_url' );?>/assets/img/ilha.png" data-lightbox="img3"
           data-title="BG Group - Ilha do Fundão">
-          <img src="assets/img/ilha.png" alt="Ilha do fundão">
+          <img src="<?php bloginfo( 'template_url' );?>/assets/img/ilha.png" alt="Ilha do fundão">
           <p class="text-item">BG Group - Ilha do fundão</p>
         </a>
       </div>
       <div class="grid-item">
-        <a href="assets/img/BR_Mania.png" data-lightbox="img4" data-title="BR Mania">
+        <a href="<?php bloginfo( 'template_url' );?>/assets/img/BR_Mania.png" data-lightbox="img4" data-title="BR Mania">
           <img
-            src="assets/img/BR_Mania.png" alt="BR Mania">
+            src="<?php bloginfo( 'template_url' );?>/assets/img/BR_Mania.png" alt="BR Mania">
             <p class="text-item">BR Mania</p>
           </a>
         </div>
       <div class="grid-item grid-item--width2">
-        <a href="assets/img/alto_pinheiros.jpg" data-lightbox="img5"
+        <a href="<?php bloginfo( 'template_url' );?>/assets/img/alto_pinheiros.jpg" data-lightbox="img5"
           data-title="CCS - Alto de Pinheiros">
-          <img src="assets/img/alto_pinheiros.jpg" alt="Alto Pinheiros">
+          <img src="<?php bloginfo( 'template_url' );?>/assets/img/alto_pinheiros.jpg" alt="Alto Pinheiros">
           <p class="text-item">CSS - Alto de Pinheiros</p>
         </a>
       </div>
       <div class="grid-item grid-item--width2">
-        <a href="assets/img/ClínicaAlus_Itapetininga.png" data-lightbox="img5"
+        <a href="<?php bloginfo( 'template_url' );?>/assets/img/ClínicaAlus_Itapetininga.png" data-lightbox="img5"
           data-title="Clínica Alus - Itapetinga">
-          <img src="assets/img/ClínicaAlus_Itapetininga.png"
+          <img src="<?php bloginfo( 'template_url' );?>/assets/img/ClínicaAlus_Itapetininga.png"
             alt="Clinica Alus">
             <p class="text-item">Clínica Alus - Itapetinga</p>
           </a>
         </div>
       <div class="grid-item">
-        <a href="assets/img/Cobasi_Tiete.png" data-lightbox="img6" data-title="Cobasi - Tiete">
-          <img src="assets/img/Cobasi_Tiete.png" alt="Cobasi Tiete">
+        <a href="<?php bloginfo( 'template_url' );?>/assets/img/Cobasi_Tiete.png" data-lightbox="img6" data-title="Cobasi - Tiete">
+          <img src="<?php bloginfo( 'template_url' );?>/assets/img/Cobasi_Tiete.png" alt="Cobasi Tiete">
           <p class="text-item">Cobasi Tiete</p>
         </a>
       </div>
       <div class="grid-item">
-        <a href="assets/img/Complexo_JK_Daslu.png" data-lightbox="img7" data-title="Complexo JK Daslu">
-          <img src="assets/img/Complexo_JK_Daslu.png" alt="Complexo JK Daslu">
+        <a href="<?php bloginfo( 'template_url' );?>/assets/img/Complexo_JK_Daslu.png" data-lightbox="img7" data-title="Complexo JK Daslu">
+          <img src="<?php bloginfo( 'template_url' );?>/assets/img/Complexo_JK_Daslu.png" alt="Complexo JK Daslu">
           <p class="text-item">Complexo JK Daslu</p>
         </a>
       </div>
       <div class="grid-item grid-item--width2">
-        <a href="assets/img/Destro_Gravatai.png" data-lightbox="img16"
+        <a href="<?php bloginfo( 'template_url' );?>/assets/img/Destro_Gravatai.png" data-lightbox="img16"
           data-title="Destro Gravatai">
-          <img src="assets/img/Destro_Gravatai.png" alt="Destro Gravatai">
+          <img src="<?php bloginfo( 'template_url' );?>/assets/img/Destro_Gravatai.png" alt="Destro Gravatai">
           <p class="text-item">Destro Gravatai</p>
         </a>
       </div>
       <div class="grid-item">
-        <a href="assets/img/Extra_Boa Viagem.png" data-lightbox="img8" data-title="Extra - Boa Viagem">
-          <img src="assets/img/Extra_Boa Viagem.png" alt="Extra - Boa Viagem">
+        <a href="<?php bloginfo( 'template_url' );?>/assets/img/Extra_Boa Viagem.png" data-lightbox="img8" data-title="Extra - Boa Viagem">
+          <img src="<?php bloginfo( 'template_url' );?>/assets/img/Extra_Boa Viagem.png" alt="Extra - Boa Viagem">
           <p class="text-item">Extra - Boa Viagem</p>
         </a>
       </div>
-      <div class="grid-item"><a href="assets/img/In_Flex_Building_Campinas.png" data-lightbox="img9"
+      <div class="grid-item"><a href="<?php bloginfo( 'template_url' );?>/assets/img/In_Flex_Building_Campinas.png" data-lightbox="img9"
           data-title="In Flex Buildings - Campinas">
-          <img src="assets/img/In_Flex_Building_Campinas.png"
+          <img src="<?php bloginfo( 'template_url' );?>/assets/img/In_Flex_Building_Campinas.png"
             alt="In Flex Building - Campinas">
             <p class="text-item">In Flex Building - Campinas</p>
             </a>
       </div>
       <div class="grid-item">
-        <a href="assets/img/LojaReboucas_Sao Paulo.jpg" data-lightbox="img10"
+        <a href="<?php bloginfo( 'template_url' );?>/assets/img/LojaReboucas_Sao Paulo.jpg" data-lightbox="img10"
           data-title="Loja Rebouças - São Paulo">
-          <img src="assets/img/LojaReboucas_Sao Paulo.jpg"
+          <img src="<?php bloginfo( 'template_url' );?>/assets/img/LojaReboucas_Sao Paulo.jpg"
             alt="Loja Reboucas - São Paulo">
             <p class="text-item">Lojas Rebouças - São Paulo</p>
           </a>
         </div>
       <div class="grid-item grid-item--width2">
-        <a href="assets/img/NovoMundo_Goiania.jpg" data-lightbox="img11"
+        <a href="<?php bloginfo( 'template_url' );?>/assets/img/NovoMundo_Goiania.jpg" data-lightbox="img11"
           data-title="Novo Mundo - Goiânia">
-          <img src="assets/img/NovoMundo_Goiania.jpg" alt="Novo Mundo - Goiânia">
+          <img src="<?php bloginfo( 'template_url' );?>/assets/img/NovoMundo_Goiania.jpg" alt="Novo Mundo - Goiânia">
           <p class="text-item">Novo Mundo - Goiânia</p>
         </a>
       </div>
       <div class="grid-item">
-        <a href="assets/img/PãodeAcucar_VilaClementino.png" data-lightbox="img12"
+        <a href="<?php bloginfo( 'template_url' );?>/assets/img/PãodeAcucar_VilaClementino.png" data-lightbox="img12"
           data-title="Pão de Açucar - Vila Clementino">
-          <img src="assets/img/PãodeAcucar_VilaClementino.png"
+          <img src="<?php bloginfo( 'template_url' );?>/assets/img/PãodeAcucar_VilaClementino.png"
             alt="Pão de Açúcar - Vila Clementino">
             <p class="text-item">Pão de Açúcar - Vila Clementino</p>
           </a>
         </div>
       <div class="grid-item">
-        <a href="assets/img/PowerCenterREP_Jundiai.jpg" data-lightbox="img12"
+        <a href="<?php bloginfo( 'template_url' );?>/assets/img/PowerCenterREP_Jundiai.jpg" data-lightbox="img12"
           data-title="Power Center REP - Jundiaí">
-          <img src="assets/img/PowerCenterREP_Jundiai.jpg"
+          <img src="<?php bloginfo( 'template_url' );?>/assets/img/PowerCenterREP_Jundiai.jpg"
             alt="Power Center REP - Jundiaí">
             <p class="text-item">Power Center REP - Jundiaí</p>
           </a>
         </div>
       <div class="grid-item grid-item--width2">
-        <a href="assets/img/SerraSulShopping_PousoAlegre.jpg"
+        <a href="<?php bloginfo( 'template_url' );?>/assets/img/SerraSulShopping_PousoAlegre.jpg"
           data-lightbox="img13" data-title="Serra Sul Shopping - Pouso Alegre">
           <img
-            src="assets/img/SerraSulShopping_PousoAlegre.jpg" alt="Serra Sul Shopping - Pouso Alegre">
+            src="<?php bloginfo( 'template_url' );?>/assets/img/SerraSulShopping_PousoAlegre.jpg" alt="Serra Sul Shopping - Pouso Alegre">
             <p class="text-item">Serra Sul Shopping - Pouso Alegre</p>
           </a>
         </div>
       <div class="grid-item grid-item--width2">
-        <a href="assets/img/ShoppingBuriti_RibeiraoPreto.jpg"
+        <a href="<?php bloginfo( 'template_url' );?>/assets/img/ShoppingBuriti_RibeiraoPreto.jpg"
           data-lightbox="img14" data-title="Shopping Buriti - Riberão Preto">
           <img
-            src="assets/img/ShoppingBuriti_RibeiraoPreto.jpg" alt="Shopping Buriti - Ribeirão Preto">
+            src="<?php bloginfo( 'template_url' );?>/assets/img/ShoppingBuriti_RibeiraoPreto.jpg" alt="Shopping Buriti - Ribeirão Preto">
             <p class="text-item">Shopping Buriti - Ribeirão Preto</p>
           </a>
         </div>
       <div class="grid-item">
-        <a href="assets/img/Sodimac_Guarulhos.jpg" data-lightbox="img15"
+        <a href="<?php bloginfo( 'template_url' );?>/assets/img/Sodimac_Guarulhos.jpg" data-lightbox="img15"
           data-title="Sodimac - Guarulhos">
-          <img src="assets/img/Sodimac_Guarulhos.jpg" alt="Sodimac - Guarulhos">
+          <img src="<?php bloginfo( 'template_url' );?>/assets/img/Sodimac_Guarulhos.jpg" alt="Sodimac - Guarulhos">
           <p class="text-item">Sodimac Guarulhos</p>
         </a>
       </div>
@@ -248,10 +254,10 @@
   <footer class="container-fluid border-top">
     <div class="row">
       <div class="col-12 d-flex flex-column pl-5 pl-lg-0 mt-4 justify-content-center align-content-center flex-lg-row">
-        <p class="img-fluid pr-5"><img class="mr-3" src="assets/img/location.svg" alt="Localização"> Rua General Jardim
+        <p class="img-fluid pr-5"><img class="mr-3" src="<?php bloginfo( 'template_url' );?>/assets/img/location.svg" alt="Localização"> Rua General Jardim
           482 - CJ 122, Vila Buarque - SP, 01223-010</p>
-        <p class="img-fluid pr-5"><img class="mr-3" src="assets/img/contato.svg" alt="Contato">11.3192-0900</p>
-        <p class="img-fluid pr-5"><img class="mr-3" src="assets/img/email.svg" alt="Email">argis@argis.com.br</p>
+        <p class="img-fluid pr-5"><img class="mr-3" src="<?php bloginfo( 'template_url' );?>/assets/img/contato.svg" alt="Contato">11.3192-0900</p>
+        <p class="img-fluid pr-5"><img class="mr-3" src="<?php bloginfo( 'template_url' );?>/assets/img/email.svg" alt="Email">argis@argis.com.br</p>
       </div>
     </div>
   </footer>
@@ -270,7 +276,6 @@
       'positionFromTop': 50
     });
   </script>
-
   <script>
     var $grid = $('.grid').isotope({
       // options
